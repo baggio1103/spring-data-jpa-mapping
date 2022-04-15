34ea1e06d9d7b655b45828936959523991b41e0d
baggio1103 <valijanov1103@mail.ru>
2022-04-16 02:45:33 +0300

    Add Student entity

---
+++ src/main/java/javajedi/com/model/Student.java
@@ -0,0 +1,34 @@
+package javajedi.com.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "student",
+        uniqueConstraints = {@UniqueConstraint(name = "student_unique_username", columnNames = "user_name"),
+                @UniqueConstraint(name = "student_unique_email", columnNames = "email")})
+@NoArgsConstructor
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "user_name", nullable = false)
+    private String username;
+
+    @Column(name = "email", nullable = false, columnDefinition = "TEXT")
+    private String email;
+
+}
---
+++ src/test/java/javajedi/com/SpringJpaMappingsApplicationTests.java
@@ -0,0 +1,13 @@
+package javajedi.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaMappingsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
